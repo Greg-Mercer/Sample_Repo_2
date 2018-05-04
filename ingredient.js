568917cd9cbd7fe395ec356c6a9de07bdcb03a0d
Unknown <Patgiok@gmail.com>
2018-05-04 16:33:53 -0700

    Ingredient list for mvp

--- ingredient.js
+++ ingredient.js
@@ -1,11 +1,14 @@
-class ingredients {
-	var price;
+class Ingredients {
+	var generic; 	//food type generic for recipes
+	var price;		//price per unit of the ingredient
 	var decayrate;  //decay rate per ingredient
 	var maxHealth;  //max health of ingredient
 	var health;     //current health of ingredient
 	var nutrition;  //nutritional value of ingredient
 	var id;         //ingredient ID
-	constructor(price, decayrate, maxHealth, health, nutrition, id){
+	constructor(name, generic, price, decayrate, maxHealth, health, nutrition, id){
+		this.name = name;
+		this.generic = generic;
 		this.price = price;
 		this.decayrate = decayrate;
 		this.maxHealth = maxHealth;
@@ -13,11 +16,31 @@ class ingredients {
 		this.nutrition = nutrition;
 		this.id = id;
 	};
+
+	function get(){
+		return new Ingredient(
+			this.name, this.generic, this.price, this.decayrate,
+			this.maxHealth, this.health, this.nutrition, this.id);
+	}
+
+	static list = [];
+
+	function start(){
+		list.push(new Ingredient("Apple", "FRUIT", 0.60, 1.00, 100, 100, 100, 0));
+		list.push(new Ingredient("Bread", "CARB", 2.80, 1.00, 100, 100, 100, 1));
+		list.push(new Ingredient("Broccoli", "VEGGIE" 3.50, 1.00, 100, 100, 100, 2));
+		list.push(new Ingredient("Carrot", "VEGGIE", 2.20, 1.00, 100, 100, 100, 3));
+	}
+
+	static function newApple("FRUIT", 0.60, 1.00, 100, 100, 100, 1){
+		return new Ingredient();
+	}
 }
 
 class Apple extends ingredients {
 	constructor() {
 		super();
+		this.generic = "FRUIT";
 		this.price = 0.60;
 		this.decayrate = 1.00;
 		this.maxHealth = 100;
@@ -30,6 +53,7 @@ class Apple extends ingredients {
 class Bread extends ingredients {
 	constructor() {
 		super();
+		this.generic = "CARB"
 		this.price = 2.80;
 		this.decayrate = 1.00;
 		this.maxHealth = 100;
@@ -43,6 +67,7 @@ class Bread extends ingredients {
 class Broccoli extends ingredients {
 	constructor() {
 		super();
+		this.generic = "VEGGIE"
 		this.price = 3.50;
 		this.decayrate = 1.00;
 		this.maxHealth = 100;
@@ -57,6 +82,7 @@ class Broccoli extends ingredients {
 class Cheese extends ingredients {
 	constructor() {
 		super();
+		this.generic = "PROTEIN"
 		this.price = 2.60;
 		this.decayrate = 1.00;
 		this.maxHealth = 100;
@@ -70,6 +96,8 @@ class Cheese extends ingredients {
 
 class Carrot extends ingredients {
 	constructor() {
+		super();
+		this.generic = "VEGGIE"
 		this.price = 2.20;
 		this.decayrate = 1.00;
 		this.maxHealth = 100;
@@ -78,45 +106,3 @@ class Carrot extends ingredients {
 		this.id = 1;
 	}
 };
-
-
-
-class Corn extends ingredients {
-	constructor() {
-		super();
-		this.price = 0.80;
-		this.decayrate = 1.00;
-		this.maxHealth = 100;
-		this.health = 100;
-		this.nutrition = 100;
-		this.id = 1;
-	}
-};
-
-
-
-class Chicken extends ingredients {
-	constructor() {
-		super();
-		this.price = 
-		this.decayrate = 1.00;
-		this.maxHealth = 100;
-		this.health = 100;
-		this.nutrition = 100;
-		this.id = 1;
-	}
-};
-
-
-
-class Kale extends ingredients {
-	constructor() {
-		super();
-		this.decayrate = 1.00;
-		this.maxHealth = 100;
-		this.health = 100;
-		this.nutrition = 100;
-		this.id = 1;
-	}
-};
-
--- meal.js
+++ meal.js
@@ -1,14 +1,30 @@
-class meal {
+class Meal {
 	var known;           //static boolean var if recipe is known
 	var ingredients[];   //array of ingredients needed
 	var amount[];        //array to check player has right amount of ingredients
 						 // to make the recipe
 	var tier;			 //Recipe tier, affects values, price etc.
 	var nutrition; 		 //nutritional value of meal
-	constructor(){};
-	function sumNutrition(ingredients[], amount[]) {
+	var id;
+
+	constructor(false, ingredients, amount, tier){
+		this.known = false;
+		this.ingredients = ingredients;
+		this.amount = amount;
+		this.tier = tier;
+		this.nutrition = sumNutrition(ingredients, amount);
+	};
+
+	static mealList = [];    
+
+	function start() {
+		mealList.push(new Meal);
+	}
+
+
+	function sumNutrition(ingredients, amount) {
 		let nutri;
-		for (i = 0; i < ingredients[].length; i++) {
+		for (i = 0; i < ingredients.length; i++) {
 			nutri += ingredients[i].nutrition * amount[i];
 		}
 		return nutri;
@@ -17,23 +33,23 @@ class meal {
 
 
 
-class kebab extends meal {
+class Kebab extends Meal {
 	constructor() {
 		super();
 		this.known = false;
-		this.ingredients[] = [Broccoli, Carrot, Corn];
-		this.amount[] = [1, 1, 1];
+		this.ingredients = [2, 1, 3];
+		this.amount = [1, 1, 1];
 		this.nutrition = ;
 		this.tier = 1;
 	}
 }
 
-class stirfry extends meal {
+class Stirfry extends Meal {
 	constructor() {
 		super();
 		this.known = false;
-		this.ingredients[] = [VEGGIE, MEAT];
-		this.amount[] = [2, 1];
+		this.ingredients = [VEGGIE, MEAT];
+		this.amount = [2, 1];
 		this.nutrition = ;
 		this.tier = 2;
 	}
